05f65c8b9ba163658f99ddae13a1f6dc8f43073f
Teepee78 <eniolatoluwalase78@gmail.com>
2023-03-09 22:06:52 +0100

    Update

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -13,7 +13,7 @@ class SessionExpAuth(SessionAuth):
         """Initialize the session"""
         try:
             self.session_duration = int(getenv('SESSION_DURATION'))
-        except ValueError:
+        except Exception:
             self.session_duration = 0
 
     def create_session(self, user_id=None):
@@ -22,13 +22,15 @@ class SessionExpAuth(SessionAuth):
         session_id = super().create_session(user_id)
         if session_id is None:
             return None
-        self.user_id_by_session_id[session_id] = {
+
+        session_dictionary = {
             "user_id": user_id,
             "created_at": datetime.now()
         }
+        self.user_id_by_session_id[session_id] = session_dictionary
         return session_id
 
-    def user_id_for_session_id(self, session_id= None):
+    def user_id_for_session_id(self, session_id=None):
         """Get user_id"""
 
         if session_id is None:
